2bb362561688b4e3630afe76f8c154d2825168dd
patrikellboj <patrikellboj@gmail.com>
2020-03-09 11:49:15 +0100

    code from johannes. Testing..

--- webshop/functions.js
+++ webshop/functions.js
@@ -1,74 +1,83 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  let productPage = document.querySelector('#products-page');
-  let cartPage = document.querySelector('#cart-page');
-  let cupcakesObj = {};
-
+class CupcakeInCart {
+  constructor(name, price, amount) {
+      this.name = name;
+      this.price = price;
+      this.amount = amount;
+  }
+}
+// Array that will contain items in cart
+let cart = [];
+document.addEventListener('DOMContentLoaded', () => {
   fetch('./cupcakes.json')
       .then(response => response.json())
-      .then(saveData)
-      .then(data => data.forEach((item, index) => {
-          console.log(item);
-          productPage.innerHTML += `  <div class="card mt-3">
+      .then(data => displayProducts(data))
+      .catch(err => console.log(err));
+});
+document.querySelector('#buyBtn').addEventListener('click', function(){
+  console.log('buy btn clicked');
+  // skicka items till local storage, samt visa nästa vy 'beställningar'
+  localStorage.setItem("cart", JSON.stringify(cart));
+  console.log(JSON.parse(localStorage.getItem("cart"))); // log test att se om vi kan dra ut rätt
+  
+  // empty cart when you buy
+  cart = [];
+  displayItemsInCart();
+  
+})
+function displayProducts(data) {
+  console.log(data);
+  let productPage = document.querySelector('#products-page');
+  data.forEach(cupcake => {
+      productPage.innerHTML += `  <div class="card mt-3">
                                           <div class="card-body">
-                                              <h5 class="card-title">${item.name}</h5>
+                                              <h5 class="card-title">${cupcake.name}</h5>
                                               <div class="float-right">
-                                                  <p class="card-text">Price: ${item.price} kr</p>
-                                                  <label for="exampleFormControlSelect1">Amount:</label>
-                                                    <input type="number" id="cupcakeNo${index}" value="1" min="1" max="10">
-                                                  <a href="#" class="btn btn-success addToCart" id="${index}">Add To Cart</a>
+                                                  <p class="card-text">Price: <span>${cupcake.price}</span> kr</p>
+                                                  <label for="cupcakeNo">Amount:</label>
+                                                  <input type="number" id="cupcakeNo" value="1" min="1" max="10">
+                                                  <br/>
+                                                  <button class="btn btn-success addToCartBtn">Add To Cart</button>
                                               </div>
                                           </div>
                                       </div>`
-      }))
-      .catch(err => console.log(err));
-
-      function saveData(dataFromJson) {
-        cupcakesObj = dataFromJson;
-        return cupcakesObj;
-      }
-
-
-      const cart = {
-        items: [],
-      };
-
-      function getNumberOfItems () {
-        return cart.items.length;
-      }
-
-      function getTotalAmount () {
-        let totalAmount = 0;
-        cart.items.forEach(item => {
-            totalAmount += item.price * item.amount;
-        });
-        return totalAmount;
-      }
-
-      function addToCart (item, amount) {
-        // TODO: kolla om varan redan finns i varukorgen
-        // om den finns -> plussa på amount och lägg inte till en ny
-        console.log('addToCart item: ' + item);
-        item.amount = amount;
-        cart.items.push(item);
-        console.log(cart);
-        updateCartView();
-      }
-
-      function updateCartView () {
-        // cartPage.innerHTML = '';
-          cart.items.forEach(e => {
-            cartPage.innerHTML += `<p>item: ${e.name}. price: ${e.price}. amount: ${e.amount}</p>`
-            });
-      }
-
-      document.addEventListener('click', function(e) {
-        if (e.target && e.target.nodeName === 'A') {
-            let amount = document.getElementById('cupcakeNo' + e.target.id).value;
-            amount = Number(amount);
-            console.log(cupcakesObj[e.target.id]);
-            addToCart(cupcakesObj[e.target.id], amount)
-        }   
-    });
-
-    
   });
+  let btns = document.querySelectorAll('.addToCartBtn');
+  btns.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+          let name = this.parentNode.parentNode.firstElementChild.innerHTML;
+          let price = parseInt(this.parentNode.firstElementChild.firstElementChild.innerHTML);
+          let amount = parseInt(this.previousElementSibling.value);
+          if (cart.some(cupcake => cupcake.name === name)) { // if cupcake already exists in cart
+              let currCupcake = cart.find(cupcake => cupcake.name === name);
+              currCupcake.amount += amount;
+          } else { // if cupcake doesn't exists in cart
+              cart.push(new CupcakeInCart(name, price, amount));
+          }
+          //reset input value to 1
+          this.previousElementSibling.value = '1';
+          displayItemsInCart();
+      });
+  })
+} // function displayProducts end
+function displayItemsInCart() {
+  //Print out all items in cart to UI and update total price
+  let cartPage = document.querySelector('#cart-page');
+  cartPage.innerHTML = '';
+  let totalPrice = 0;
+  cart.forEach(cupcake => {
+      cartPage.innerHTML += `  <div class="card mb-3">
+                                          <div class="card-body">
+                                              <h5 class="card-title">${cupcake.name}</h5>
+                                              <div class="float-right">
+                                                  <p class="card-text">
+                                                  Amount: <span> ${cupcake.amount} st <br/>
+                                                  Price: <span>${cupcake.price * cupcake.amount}</span> kr 
+                                                  </p>
+                                              </div>
+                                          </div>
+                                      </div>`
+      
+      totalPrice += (cupcake.price * cupcake.amount);
+  });
+  document.querySelector('#cart-total').innerHTML = totalPrice;
+}
